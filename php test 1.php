c7002d9bf30088bf3492f85f195ac791081ad0ea
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-26 23:59:17 +0530

    Add files via upload

---
+++ php test 1.php
@@ -0,0 +1,13 @@
+//oshe ravanz contribution
+//oshada97
+
+<html>
+    <head>
+        <title>My First PHP Page</title> <!--html codes with commenting style in html-->
+    </head>
+    <body>
+    <?php   
+        echo "Hello World!";////"echo" function is a use to print,display output 
+    ?>
+    </body>
+</html>
